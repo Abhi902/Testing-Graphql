eb61b063ecd6664a36cf727f8c306cd1107a2b71
Abhi902 <dangiabhishek336@gmail.com>
2022-07-27 22:04:24 +0530

    fourth commit

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,32 @@ class _LoginScreenState extends State<LoginScreen>
       appBar: AppBar(
         title: Text('testing'),
       ),
-      body: mutation(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            'INDIA',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'USA',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text(
+            'Canada',
+            style: TextStyle(
+                fontSize: 30, color: Colors.black, fontWeight: FontWeight.bold),
+          )
+        ],
+      ),
     );
   }
 }
